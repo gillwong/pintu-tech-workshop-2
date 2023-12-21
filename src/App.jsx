c0741ec2b5694d401813c50c3ac9c81ce32f0aa4
Gillbert Wong <66062290+gillwong@users.noreply.github.com>
2023-12-21 17:22:40 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+// List of items
+const items = [
+  {
+    name: "Handphone 1",
+    price: 399,
+    image: "images/img1.png",
+    category: "Electronics",
+  },
+  {
+    name: "Handphone 2",
+    price: 459,
+    image: "images/img2.png",
+    category: "Electronics",
+  },
+  {
+    name: "Handphone 3",
+    price: 249,
+    image: "images/img3.png",
+    category: "Electronics",
+  },
+  {
+    name: "Handphone 4",
+    price: 379,
+    image: "images/img4.png",
+    category: "Electronics",
+  },
+  {
+    name: "Handphone 5",
+    price: 799,
+    image: "images/img5.png",
+    category: "Electronics",
+  },
+  {
+    name: "Sepatu 1",
+    price: 99,
+    image: "images/img6.png",
+    category: "Clothes",
+  },
+  {
+    name: "Sepatu 2",
+    price: 89,
+    image: "images/img7.png",
+    category: "Clothes",
+  },
+  {
+    name: "Sepatu 3",
+    price: 129,
+    image: "images/img8.png",
+    category: "Clothes",
+  },
+  {
+    name: "Sepatu 4",
+    price: 79,
+    image: "images/img9.png",
+    category: "Clothes",
+  },
+  {
+    name: "Sepatu 5",
+    price: 99,
+    image: "images/img10.png",
+    category: "Clothes",
+  },
+];
+// Jangan Edit Code di Bagian Atas Ini
+// Tulis kode kalian di bawah ini
+
+function App() {
+  return <main>Hello World!</main>;
+}
+
+export default App;
